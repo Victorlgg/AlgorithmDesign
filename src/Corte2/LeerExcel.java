2f9f43cb66bad25d5d1426696d940fd51b32936b
Victor <Victor@Victor-PC>
2019-10-31 11:08:45 -0500

    Dijsktra cc cv

--- src/Corte2/LeerExcel.java
+++ src/Corte2/LeerExcel.java
@@ -10,6 +10,7 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -22,33 +23,37 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * @author Victor
  */
 public class LeerExcel {
-    int[][] MatrizAdy;
-    
-    public LeerExcel(File Ruta){
-    List cellData = new ArrayList();
-    
+
+    static int[][] MatrizAdy;
+
+    public LeerExcel() {
+    }
+
+    public LeerExcel(File Ruta, int hoja) {
+        List cellData = new ArrayList();
+
         try {
             FileInputStream Archivo = new FileInputStream(Ruta);
             //Leer Excel
             XSSFWorkbook libroExcel = new XSSFWorkbook(Archivo);
             //obtener la hoja que se va leer
-            XSSFSheet hojaExcel = libroExcel.getSheetAt(0);
+            XSSFSheet hojaExcel = libroExcel.getSheetAt(hoja);
             //Obtener todas las filas de la hoja excel
             Iterator<Row> rowIterador = hojaExcel.iterator();
-            
+
             Row row;
             //Recorrer cada Fila
-            while(rowIterador.hasNext()){
-                row=rowIterador.next();
+            while (rowIterador.hasNext()) {
+                row = rowIterador.next();
                 //Se obtiene las celdas por fila
-                Iterator<Cell> cellIterator= row.cellIterator();
+                Iterator<Cell> cellIterator = row.cellIterator();
                 Cell cell;
                 List cellTemp = new ArrayList();
                 //Se recorre la celda
-                while(cellIterator.hasNext()){
+                while (cellIterator.hasNext()) {
                     //Se obtiene la celda en especifico y se imprime
                     cell = cellIterator.next();
-                    cellTemp.add((int)cell.getNumericCellValue());
+                    cellTemp.add((int) cell.getNumericCellValue());
 //                    System.out.print((int)cell.getNumericCellValue()+" ");
                 }
 //                System.out.println();
@@ -57,68 +62,85 @@ public class LeerExcel {
         } catch (Exception e) {
             e.getMessage();
         }
+
         agregarMatrizAdy(cellData);
     }
-    
-    private void agregarMatrizAdy(List cellDataList){
+
+    private void agregarMatrizAdy(List cellDataList) {
         int x = cellDataList.size();
         MatrizAdy = new int[x][x];
-        for(int i=0;i<cellDataList.size();i++){
+        for (int i = 0; i < cellDataList.size(); i++) {
             List cellTempList = (List) cellDataList.get(i);
-            for(int j=0; j<cellTempList.size();j++){
+            for (int j = 0; j < cellTempList.size(); j++) {
 //            XSSFCell hssfCell = (XSSFCell) cellTempList.get(j);
-             MatrizAdy[i][j]=(int) cellTempList.get(j);
-            //cell.getNumericCellValue(),
+                MatrizAdy[i][j] = (int) cellTempList.get(j);
+                //cell.getNumericCellValue(),
 //            String stringCellValue = hssfCell.toString();
-            System.out.print(MatrizAdy[i][j]>9 ? MatrizAdy[i][j]+" ":"0"+MatrizAdy[i][j]+" ");
+                System.out.print(MatrizAdy[i][j] > 9 ? MatrizAdy[i][j] + " " : "0" + MatrizAdy[i][j] + " ");
 //                System.out.print( j!=cellTempList.size()-1 ? ""+MatrizAdy[i][j]+",":MatrizAdy[i][j]);
             }
             System.out.println();
         }
-        MultiplicarMatriz(MatrizAdy);
+
     }
-    
-    public void MultiplicarMatriz(int[][] MatrizAdy){
-    MultiMatriz M = new MultiMatriz();
-    for(int i=0;i<MatrizAdy.length;i++){
-           for(int j=0;j<MatrizAdy.length;j++){
-            
-            System.out.print(MatrizAdy[i][j]>=10 && MatrizAdy[i][j]<100 ? "["+MatrizAdy[i][j]+"]":"");
-            System.out.print(MatrizAdy[i][j]<10? "[0"+MatrizAdy[i][j]+"]":"");
-               
-             }
-           System.out.println();
-        }
-    
+
+    public static void MultiplicarMatriz(int[][] MatrizAdy) {
+        MultiMatriz M = new MultiMatriz();
+//    for(int i=0;i<MatrizAdy.length;i++){
+//           for(int j=0;j<MatrizAdy.length;j++){
+//            
+//            System.out.print(MatrizAdy[i][j]>=10 && MatrizAdy[i][j]<100 ? "["+MatrizAdy[i][j]+"]":"");
+//            System.out.print(MatrizAdy[i][j]<10? "[0"+MatrizAdy[i][j]+"]":"");
+//               
+//             }
+//           System.out.println();
+//        }
         int Multiplicacion[][] = M.matrixMultiplicationFinal(MatrizAdy, MatrizAdy);
-        for(int i=0;i<Multiplicacion.length;i++){
-           for(int j=0;j<Multiplicacion.length;j++){
-            System.out.print(Multiplicacion[i][j]>=100 ? "["+Multiplicacion[i][j]+"]":"");
-            System.out.print(Multiplicacion[i][j]>10 && Multiplicacion[i][j]<100 ? "[0"+Multiplicacion[i][j]+"]":"");
-            System.out.print(Multiplicacion[i][j]<9? "[00"+Multiplicacion[i][j]+"]":"");
-               
-             }
-           System.out.println();
+        for (int i = 0; i < Multiplicacion.length; i++) {
+            for (int j = 0; j < Multiplicacion.length; j++) {
+                System.out.print(Multiplicacion[i][j] >= 100 ? "[" + Multiplicacion[i][j] + "]" : "");
+                System.out.print(Multiplicacion[i][j] > 10 && Multiplicacion[i][j] < 100 ? "[0" + Multiplicacion[i][j] + "]" : "");
+                System.out.print(Multiplicacion[i][j] < 9 ? "[00" + Multiplicacion[i][j] + "]" : "");
+            }
+            System.out.println();
         }
-            System.out.println(" -------------------------------");
-        int[][] Mcuadrada = M.multiply(MatrizAdy, MatrizAdy);
-        for(int i=0;i<Mcuadrada.length;i++){
-           for(int j=0;j<Mcuadrada.length;j++){
-            System.out.print(Mcuadrada[i][j]>=100 ? "["+Mcuadrada[i][j]+"]":"");
-            System.out.print(Mcuadrada[i][j]>10 && Mcuadrada[i][j]<100 ? "[0"+Mcuadrada[i][j]+"]":"");
-            System.out.print(Mcuadrada[i][j]<9? "[00"+Mcuadrada[i][j]+"]":"");
-               
-             }
-           System.out.println();
-        }
-        
-        
     }
-    
-    public static void main(String [] args){
+
+    public static void MultiplicarMatrizCuadrada(int[][] MatrizAdy) {
+        MultiMatriz M = new MultiMatriz();
+        int[][] Mcuadrada = M.multiply(MatrizAdy, MatrizAdy);
+        for (int i = 0; i < Mcuadrada.length; i++) {
+            for (int j = 0; j < Mcuadrada.length; j++) {
+                System.out.print(Mcuadrada[i][j] >= 100 ? "[" + Mcuadrada[i][j] + "]" : "");
+                System.out.print(Mcuadrada[i][j] > 10 && Mcuadrada[i][j] < 100 ? "[0" + Mcuadrada[i][j] + "]" : "");
+                System.out.print(Mcuadrada[i][j] < 9 ? "[00" + Mcuadrada[i][j] + "]" : "");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
         File f = new File("./src/ArchivosC2/MatrizGrafo.xlsx");
-        if(f.exists()){
-            LeerExcel obj = new LeerExcel(f);
+        if (f.exists()) {
+            System.out.println("0.Matriz Adyacencia, 1.Matriz Multiplicar");
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            LeerExcel obj2 = new LeerExcel();
+            LeerExcel obj = new LeerExcel(f, x);
+
+            switch (x) {
+                case 0:
+                    Dijkstra D = new Dijkstra();
+                    D.dijkstra(MatrizAdy, 0);
+                    break;
+                case 1:
+                    MultiplicarMatriz(MatrizAdy);
+                    MultiplicarMatrizCuadrada(MatrizAdy);
+                    break;
+
+            }
+
         }
     }
 }
